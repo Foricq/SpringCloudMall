d6a8a5dc7b0d0c38625d7c387d7bc0f675ffc9c3
曹晓春 <xiaochun.cao@changhong.com>
2019-09-08 21:16:35 +0800

    eureka

--- eureka/src/main/java/com/bigch/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/bigch/eureka/EurekaApplication.java
@@ -2,7 +2,14 @@ package com.bigch.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author: create by cxc
+ * @description: 注册中心
+ * @date:2019/9/8
+*/
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
